f9a70a418bb0026e31cb3d3a483fce4ffa2006e5
Troy Day <troy.day@polarisalpha.com>
2020-01-01 17:41:18 -0700

    Fixes #9: Plus enhancements to improve po box handling

--- src/pOBox.js
+++ src/pOBox.js
@@ -1,7 +1,7 @@
 //js lib supporting a macro by a similar file name
 window.cleanUp.extractPOBox=function(a1,z){
     var ad1=a1.value.split(' ');
-    var hyphen=z.length?'-':'';
+    var hyphen=z.value.length?'-':'';
     var pb = '';
     if(ad1[0]==='PO'&&ad1[1]==='Box'){
         pb=z.value.substring(0,5)+hyphen+ad1[2].padStart(4, '0');
